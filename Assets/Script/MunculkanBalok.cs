0fd3bc1708cf237d3170da777edccacb517e7740
Evander Gabriel <93313423+evangab123@users.noreply.github.com>
2023-03-31 14:03:13 +0700

    Selesai

---
+++ Assets/Script/MunculkanBalok.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MunculkanBalok : MonoBehaviour{
+    public float jeda = 0.8f;
+    float timer;
+    public GameObject[] obyekBalok;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > jeda)
+        {
+        int random = Random.Range(0, obyekBalok.Length);
+        Instantiate(obyekBalok[random], transform.position, transform.rotation);
+        timer = 0;
+        }
+    }
+}
